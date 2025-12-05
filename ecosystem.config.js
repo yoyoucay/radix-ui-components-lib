1489554f2a0ad87911a6d146964dfca8298fe31b
Arif Febriana <arif.febriana@yageo.com>
2025-12-05 14:51:12 +0700

    feat: Refactor ecosystem configuration and update build scripts for improved deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,30 +12,17 @@ module.exports = {
       watch: false,
       max_memory_restart: '512M',
       env: {
-        NODE_ENV: process.env.NODE_ENV || 'DEV',
-        PORT: process.env.PORT || 3000,
-        BASE_PATH: process.env.BASE_PATH || '/'
-      },
-      env_production: {
-        NODE_ENV: process.env.NODE_ENV ||  'PRD' ,
-        PORT: process.env.PORT || 3000,
-        BASE_PATH: process.env.BASE_PATH || '/'
-      }
-    },
-    {
-      name: 'ui-kit',
-      script: 'bun',
-      args: 'run start',
-      cwd: './',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '512M',
-      env: {
         NODE_ENV: 'production',
-        PORT: process.env.PREVIEW_PORT || 4173,
+        PORT: process.env.PORT || 3000,
         BASE_PATH: process.env.BASE_PATH || '/'
       }
     }
-  ]
+  ],
+  deploy: {
+    production: {
+      'pre-deploy-local': 'echo "Starting deployment"',
+      'post-deploy': 'bun install && bun run deploy && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': 'echo "Setting up deployment"'
+    }
+  }
 };
