7dcebdf5a39e8d306b1e2ca3a8333399fe25cb29
Arif Febriana <arif.febriana@yageo.com>
2025-12-05 14:37:39 +0700

    feat: Add new environment configuration for ui-kit application

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,8 +12,29 @@ module.exports = {
       watch: false,
       max_memory_restart: '512M',
       env: {
+        NODE_ENV: process.env.NODE_ENV || 'DEV',
+        PORT: process.env.PORT || 3000,
+        BASE_PATH: process.env.BASE_PATH || '/'
+      },
+      env_production: {
+        NODE_ENV: process.env.NODE_ENV ||  'PRD' ,
+        PORT: process.env.PORT || 3000,
+        BASE_PATH: process.env.BASE_PATH || '/'
+      }
+    },
+    {
+      name: 'ui-kit',
+      script: 'bun',
+      args: 'run start',
+      cwd: './',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '512M',
+      env: {
         NODE_ENV: 'production',
-        PORT: process.env.PORT || 3001
+        PORT: process.env.PREVIEW_PORT || 4173,
+        BASE_PATH: process.env.BASE_PATH || '/'
       }
     }
   ]
